2ab1ad1e6aa0897a19b4d2facad590929d73942a
antoniastere90 <antoniastere90@gmail.com>
2024-09-05 20:55:02 +0300

    Create report.py

---
+++ report.py
@@ -0,0 +1,14 @@
+def main():
+    spacecraft = {"name"  :  }
+    ...
+
+
+def create_report(spacecraft):
+     return f"""
+========= REPORT =========
+
+  Name: TODO
+  Distance: TODO
+
+===========================
+"""
